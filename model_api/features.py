96599b2927e71676a928cd58b6f9381f637399a9
Kzis <driftkao@gmail.com>
2022-04-24 00:09:17 +0700

    add:api features

--- model_api/features.py
+++ model_api/features.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from utility_package.model_utils import *
+from utility_package.csv_utlity import *
+from typing import Optional
 
 router = APIRouter(
     prefix="/features",
@@ -8,5 +9,19 @@ router = APIRouter(
 )
 
 @router.get("/")
-def get_features():
-    return {"test" : "xxxx"}
+def get_features(user_id: Optional[int] = None):
+    response = {}
+    histories_list=[]
+    try:
+        if user_id :
+            csv_utils = CSVUtils()
+            list_ratings = csv_utils.read_ratings(user_id)
+            histories = {"histories": list_ratings}
+            histories_list.append(histories)
+            response = {"features":histories_list}
+        else:
+            response = {"desciption": "user_id is required"}
+    except Exception as err:
+        print(err)
+    finally:
+        return response
